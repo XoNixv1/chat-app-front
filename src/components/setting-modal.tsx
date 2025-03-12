774d1e4ab90345bd73dec4645cfda2c2299562a5
XoNixv1 <haled14253@gmail.com>
2025-03-12 20:35:43 +0100

    storage aproach, bugs fixed

--- src/components/setting-modal.tsx
+++ src/components/setting-modal.tsx
@@ -98,6 +98,8 @@ export default function SettingModal({
     router.push("/login");
   };
 
+  console.log(initialData?.currentUser);
+
   return (
     <div className="fixed inset-0 z-50 bg-black/50 flex items-center justify-center p-4">
       <div
